1764bb85dadd8557045ede038156539422a76426
abrarnaim015 <aabrarnaim24@gmail.com>
2020-09-25 01:08:57 +0700

    tahap 1

---
+++ js.js
@@ -0,0 +1,39 @@
+// // let trNami = document.getElementsByClassName('game3')
+// // if(trNami===trueGame3)
+// // {
+// //     console.log(true)
+// // }
+// // else if(trNami===falseGame3)
+// // {
+// //     console.log(false)
+// // }
+
+// let nami2 = document.getElementById('jwbGame3benar')
+// // addEventListener("click", console.log('lala'))
+// // // console.log(nami)
+// // function myFunction() {
+// //     alert('Hello');
+// //   }
+// let nami4 = document.getElementById('jwbGame3salah')
+// addEventListener('onclick', console.log('asfa'))
+// console.log(nami4)
+
+// console.log(nami2)
+// if(nami2==='mama')
+// {
+//     console.log('papa')
+// }
+
+// let mama6 = 'papa'
+
+// let sasa = document.getElementsByClassName('game3')
+// console.log(sasa)
+
+// nami8 = document.getElementById('jwbGame3benar')
+// console.log(document.getElementById('jwbGame3benar'))
+// console.log(document.getElementsByClassName('game3'))
+// console.log(document.getElementsByClassName('game3'))
+// console.log(document.getElementsByClassName('jwbGame3benar'))
+
+let audio = document.getElementById('musik').autoplay
+// document

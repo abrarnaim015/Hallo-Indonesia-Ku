eb128e2503cb57414f16e05164c51a27955eef95
abrarnaim015 <aabrarnaim24@gmail.com>
2020-09-25 11:20:57 +0700

    tahap 3(menambahkan DOM gambar)

---
+++ js.js
@@ -0,0 +1,54 @@
+function jwbluffy()
+{
+    let luffyB = document.getElementsByClassName("luffyT")
+    luffyB[0].classList.remove("display-none")
+}
+
+function jwbzoro()
+{
+    let zoroB = document.getElementsByClassName("zoroT")
+    zoroB[0].classList.remove("display-none")
+}
+
+function jwbnami() 
+{
+    let namiB = document.getElementsByClassName("namiT");
+    namiB[0].classList.remove("display-none");
+    
+}
+
+function jwbusopp() 
+{
+    let usoppB = document.getElementsByClassName("usoppT");
+    usoppB[0].classList.remove("display-none");
+}
+
+function jwbsanji() 
+{
+    let sanjiB = document.getElementsByClassName("sanjiT");
+    sanjiB[0].classList.remove("display-none");
+}
+
+function jwbchopper() 
+{
+    let chopperB = document.getElementsByClassName("chopperT");
+    chopperB[0].classList.remove("display-none");
+}
+
+function jwbrobin() 
+{
+    let robinB = document.getElementsByClassName("robinT");
+    robinB[0].classList.remove("display-none");
+}
+
+function jwbfranky() 
+{
+    let frankyB = document.getElementsByClassName("frankyT");
+    frankyB[0].classList.remove("display-none");
+}
+
+function jwbbrook() 
+{
+    let brookB = document.getElementsByClassName("brookT");
+    brookB[0].classList.remove("display-none");
+}

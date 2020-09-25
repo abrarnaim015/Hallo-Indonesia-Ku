56d2bbb4647367c7231d50adda053764a5b73d45
abrarnaim015 <aabrarnaim24@gmail.com>
2020-09-25 16:27:51 +0700

    tahap 6(80%)

--- js.js
+++ js.js
@@ -52,3 +52,25 @@ function jwbbrook()
     let brookB = document.getElementsByClassName("brookT");
     brookB[0].classList.remove("display-none");
 }
+
+function bonus1() 
+{
+    let b1 = document.getElementsByClassName("imgBonus1");
+    b1[0].classList.remove("display-none");
+}
+
+function bonus3() 
+{
+    let b3 = document.getElementsByClassName("imgBonus3");
+    b3[0].classList.remove("display-none");
+    b3[1].classList.remove("display-none");
+    b3[2].classList.remove("display-none");
+}
+
+function bonus7() 
+{
+    let b7 = document.getElementsByClassName("imgBonus7");
+    b7[0].classList.remove("display-none");
+    b7[1].classList.remove("display-none");
+    b7[2].classList.remove("display-none");
+}
